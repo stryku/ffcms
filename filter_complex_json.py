dba9a95a4e3f6adbb2c4a55ebb0b5fdbc121940d
stryku <stryku2393@gmail.com>
2020-03-21 16:12:53 +0100

    Init commit

---
+++ filter_complex_json.py
@@ -0,0 +1,48 @@
+import json
+
+
+"""
+ffmpeg -y -i shord_behemot.mov -i shord_behemot.mov -i shord_behemot.mov -i shord_behemot.mov -filter_complex \      
+"[1:v]negate[a]; \
+ [2:v]hflip[b]; \
+ [3:v]edgedetect[c]; \
+ [0:v][a]hstack=inputs=2[top]; \
+ [b][c]hstack=inputs=2[bottom]; \
+ [top][bottom]vstack=inputs=2[out]" -map "[out]" -c:v ffv1  multiple_input_grid.avi
+"""
+
+
+
+class FilterComplexBuilder:
+    def __init__(self):
+        self._filters = []
+
+    def with_filter(self, f):
+        self._filters.append(f)
+
+    def build(self):
+        return ';'.join(self._filters)
+
+        
+def json_to_filter_complex(json_str):
+    builder = FilterComplexBuilder()
+
+    definitions = json.loads(json_str)
+
+    filter_template = '{inputs}{filter}{output}'
+
+    i = 0
+    for input_id, input_file in definitions['inputs']:
+        raw_input = '[{}:v]'.format(i)
+        output_id = input_id
+        filter = filter_template.format(inputs=raw_input, filter='', output=output_id)
+        builder.with_filter(filter)
+
+    return builder.build()
+
+
+test_json = open('test.json', 'r').read()
+
+result = json_to_filter_complex(test_json)
+print(result)
+
